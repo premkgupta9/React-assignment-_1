24d23f9c751145e5f0f834a32c52893a7adacd6c
premkgupta <premg9554@gmial.com>
2023-09-23 11:01:53 +0530

    pw skills react assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Person from './Person';
+import Button from './Button';
+import Header from './Header';
+import List from './List';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<>
+
+   <Person name="Precost" age="20" />
+   <Button text="Click here!" onClick = {() => console.log("button clicked")} />
+   <Header title="Welcome to react!" />
+   <List items={['apple', 'banana', ]} />
+    </>
   );
 }
 
---
+++ src/Button.js
@@ -0,0 +1,10 @@
+ /*2 **/
+ function Button(props) {
+    return (
+      <button onClick = {props.onClick}>
+        {props.text}
+      </button>
+    );
+  }
+
+  export default Button
